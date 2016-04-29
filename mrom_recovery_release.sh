98a91c6d0984625a8ce6cade29a67ab5d698f0da
Nolen Johnson <johnsonnolen@gmail.com>
2016-04-28 22:56:39 -0400

    mrom_recovery_release.sh: change for my personal use (IS MESSY, WON'T WORK FOR YOU WITHOUT EDITS)

--- mrom_recovery_release.sh
+++ mrom_recovery_release.sh
@@ -1,56 +1,37 @@
 #!/bin/sh
-TMP="/tmp/"
 
 if [ -z "$TARGET_DEVICE" ]; then
     TARGET_DEVICE=$(basename $OUT)
 fi
 
-OTHER=""
 TAG="$TARGET_DEVICE"
 
-case $TARGET_DEVICE in
-    flo)
-        OTHER="deb"
-        ;;
-    grouper)
-        OTHER="tilapia"
-        ;;
-esac
-
-DEST_DIR="/home/tassadar/nexus/multirom/$TAG/"
-DEST_DIR_OTHER="/home/tassadar/nexus/multirom/$OTHER/"
+DEST_DIR="/home/nolenjohnson/Git/multirom/update-multirommgr-manifest/$TARGET_DEVICE/"
+DEST_DIR_OTHER="/home/nolenjohnson/Git/multirom/update-multirommgr-manifest/$TARGET_DEVICE/"
 if [ -z "$IMG_PATH" ]; then
-    IMG_PATH="/home/tassadar/android/android-repo-om/out/target/product/$TAG/recovery.img"
+    IMG_PATH="/home/nolenjohnson/omni/out/target/product/$TAG/recovery.img"
 fi
 
 if [ "$RECOVERY_SUBVER" = "" ]; then
-    RECOVERY_SUBVER="00"
+    RECOVERY_SUBVER="STABLE6"
 fi
 
-if [ "$#" -ge "1" ]; then
-    DEST_NAME="TWRP_multirom_${TAG}_$(date +%Y%m%d)-$1.img"
-elif [ "$RECOVERY_SUBVER" != "00" ]; then
-    DEST_NAME="TWRP_multirom_${TAG}_$(date +%Y%m%d)-$RECOVERY_SUBVER.img"
+if [ "$RECOVERY_SUBVER" != "00" ]; then
+    DEST_NAME="mr-twrp-recovery-$(date +%m%d%Y)-$RECOVERY_SUBVER.img"
 else
-    DEST_NAME="TWRP_multirom_${TAG}_$(date +%Y%m%d).img"
+    DEST_NAME="mr-twrp-recovery-$(date +%m%d%Y).img"
 fi
 
-if [ "$AUTO_PATCH_INCREMENT" = "true" ]; then
-    while [ -f "$DEST_DIR/$DEST_NAME" ] && [ "$RECOVERY_SUBVER" -lt "60" ]; do
-        RECOVERY_SUBVER=$(printf "%02d" $((RECOVERY_SUBVER+1)) )
-        DEST_NAME="TWRP_multirom_${TAG}_$(date +%Y%m%d)-$RECOVERY_SUBVER.img"
-    done
+if [ -d "~/tmp/mrom_recovery_release" ]; then
+    rm -r ~/tmp/mrom_recovery_release || exit 1
 fi
-
-if [ -d "$TMP/mrom_recovery_release" ]; then
-    rm -r $TMP/mrom_recovery_release || exit 1
-fi
-mkdir $TMP/mrom_recovery_release
-cd $TMP/mrom_recovery_release
+mkdir ~/tmp/
+mkdir ~/tmp/mrom_recovery_release/
+cd ~/tmp/mrom_recovery_release/
 
 cp -a $IMG_PATH ./
 
-if [ -n "$OTHER" ]; then
+if [ -n "$TARGET_DEVICE" ]; then
     bbootimg -x ./$(basename "$IMG_PATH") >/dev/null 2>&1 || exit 1
 
     if lzma -S .img -t initrd.img >/dev/null 2>&1; then
@@ -61,7 +42,7 @@ if [ -n "$OTHER" ]; then
         CMPR="gzip"
     else
         echo "Failed to identify ramdisk compression!"
-        rm -r $TMP/mrom_recovery_release
+        rm -r ~/tmp/mrom_recovery_release
         exit 1
     fi
 
@@ -70,13 +51,13 @@ if [ -n "$OTHER" ]; then
 
     $DCMPR ../initrd.img | cpio -i >/dev/null 2>&1
 
-    sed -i -e "s/ro.build.product=$TAG/ro.build.product=$OTHER/g" default.prop
-    sed -i -e "s/ro.product.device=$TAG/ro.product.device=$OTHER/g" default.prop
+    sed -i -e "s/ro.build.product=$TAG/ro.build.product=$TARGET_DEVICE/g" default.prop
+    sed -i -e "s/ro.product.device=$TAG/ro.product.device=$TARGET DEVICE/g" default.prop
 
     find | sort | cpio --quiet -o -H newc | $CMPR > ../initrd.img
     cd ..
 
-    DEST_NAME_OTHER="TWRP_multirom_${OTHER}_$(date +%Y%m%d)"
+    DEST_NAME_OTHER="mr-twrp-recovery-$(date +%m%d%Y)"
     if [ "$RECOVERY_SUBVER" != "00" ]; then
         DEST_NAME_OTHER="${DEST_NAME_OTHER}-${RECOVERY_SUBVER}.img"
     else
@@ -84,7 +65,7 @@ if [ -n "$OTHER" ]; then
     fi
 
     grep -v "bootsize" bootimg.cfg > bootimg-new.cfg
-    bbootimg --create "$DEST_DIR_OTHER/$DEST_NAME_OTHER" -f bootimg-new.cfg -c "name = mrom$(date +%Y%m%d)-$RECOVERY_SUBVER" -k zImage -r initrd.img >/dev/null 2>&1 || exit 1
+    bbootimg --create "$DEST_DIR_OTHER/$DEST_NAME_OTHER" -f bootimg-new.cfg -c "name = mrom$(date +%m%d%Y)-$RECOVERY_SUBVER" -k zImage -r initrd.img >/dev/null 2>&1 || exit 1
     if [ "$PRINT_FILES" = "true" ]; then
         printf "${DEST_DIR_OTHER}${DEST_NAME_OTHER} "
     else
@@ -92,13 +73,14 @@ if [ -n "$OTHER" ]; then
     fi
 fi
 
-bbootimg -u $(basename "$IMG_PATH") -c "name = mrom$(date +%Y%m%d)-$RECOVERY_SUBVER" >/dev/null 2>&1 || exit 1
+bbootimg -u $(basename "$IMG_PATH") -c "name = mrom$(date +%m%d%Y)-$RECOVERY_SUBVER" >/dev/null 2>&1 || exit 1
 cp ./$(basename "$IMG_PATH") "${DEST_DIR}/$DEST_NAME"
 
-rm -r $TMP/mrom_recovery_release
+rm -r ~/tmp/mrom_recovery_release
 
 if [ "$PRINT_FILES" = "true" ]; then
     printf "${DEST_DIR}${DEST_NAME}\n"
 else
     md5sum "$DEST_DIR/$DEST_NAME"
 fi
+echo done
